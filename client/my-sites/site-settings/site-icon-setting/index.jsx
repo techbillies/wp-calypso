0c0512ac72ae8819365375c8f8f7dc0ee404125a
Andrew Duthie <andrew@andrewduthie.com>
2016-11-11 14:23:16 -0500

    Site Settings: Render site icon modal when enabled

--- client/my-sites/site-settings/site-icon-setting/index.jsx
+++ client/my-sites/site-settings/site-icon-setting/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
@@ -10,6 +10,7 @@ import { localize } from 'i18n-calypso';
  */
 import SiteIcon from 'components/site-icon';
 import Button from 'components/button';
+import SiteIconMediaModal from './site-icon-media-modal';
 import { isJetpackSite, getCustomizerUrl, getSiteAdminUrl } from 'state/sites/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { isEnabled } from 'config';
@@ -18,44 +19,66 @@ import FormLabel from 'components/forms/form-label';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import { addQueryArgs } from 'lib/url';
 
-function SiteIconSetting( { translate, siteId, isJetpack, customizerUrl, generalOptionsUrl } ) {
-	let buttonProps;
-	if ( ! isEnabled( 'manage/site-settings/site-icon' ) ) {
-		buttonProps = { rel: 'external' };
+class SiteIconSetting extends Component {
+	static propTypes = {
+		translate: PropTypes.func,
+		siteId: PropTypes.number,
+		isJetpack: PropTypes.bool,
+		customizerUrl: PropTypes.string,
+		generalOptionsUrl: PropTypes.string
+	};
 
-		if ( isJetpack ) {
-			buttonProps.href = addQueryArgs( {
-				'autofocus[section]': 'title_tagline'
-			}, customizerUrl );
+	state = {
+		isModalVisible: false
+	};
+
+	toggleModal = ( isModalVisible ) => this.setState( { isModalVisible } );
+
+	hideModal = () => this.toggleModal( false );
+
+	showModal = () => this.toggleModal( true );
+
+	render() {
+		const { translate, siteId, isJetpack, customizerUrl, generalOptionsUrl } = this.props;
+		const isIconManagementEnabled = isEnabled( 'manage/site-settings/site-icon' );
+
+		let buttonProps;
+		if ( isIconManagementEnabled ) {
+			buttonProps = { onClick: this.showModal };
 		} else {
-			buttonProps.href = generalOptionsUrl;
-			buttonProps.target = '_blank';
+			buttonProps = { rel: 'external' };
+
+			if ( isJetpack ) {
+				buttonProps.href = addQueryArgs( {
+					'autofocus[section]': 'title_tagline'
+				}, customizerUrl );
+			} else {
+				buttonProps.href = generalOptionsUrl;
+				buttonProps.target = '_blank';
+			}
 		}
+
+		return (
+			<FormFieldset className="site-icon-setting">
+				<FormLabel>{ translate( 'Site Icon' ) }</FormLabel>
+				<div className="site-icon-setting__controls">
+					<SiteIcon size={ 64 } siteId={ siteId } />
+					<Button { ...buttonProps } className="site-icon-setting__change-button">
+						{ translate( 'Change site icon' ) }
+					</Button>
+				</div>
+				<FormSettingExplanation>
+					{ translate( 'The Site Icon is used as a browser and app icon for your site.' ) }
+				</FormSettingExplanation>
+				{ isIconManagementEnabled && (
+					<SiteIconMediaModal
+						visible={ this.state.isModalVisible }
+						onClose={ this.hideModal } />
+				) }
+			</FormFieldset>
+		);
 	}
-
-	return (
-		<FormFieldset className="site-icon-setting">
-			<FormLabel>{ translate( 'Site Icon' ) }</FormLabel>
-			<div className="site-icon-setting__controls">
-				<SiteIcon size={ 64 } siteId={ siteId } />
-				<Button { ...buttonProps } className="site-icon-setting__change-button">
-					{ translate( 'Change site icon' ) }
-				</Button>
-			</div>
-			<FormSettingExplanation>
-				{ translate( 'The Site Icon is used as a browser and app icon for your site.' ) }
-			</FormSettingExplanation>
-		</FormFieldset>
-	);
 }
-
-SiteIconSetting.propTypes = {
-	translate: PropTypes.func,
-	siteId: PropTypes.number,
-	isJetpack: PropTypes.bool,
-	customizerUrl: PropTypes.string,
-	generalOptionsUrl: PropTypes.string
-};
 
 export default connect( ( state ) => {
 	const siteId = getSelectedSiteId( state );
