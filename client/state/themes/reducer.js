ffd8d02c13adc6757ea2015d305550ec0750dc21
budzanowski <bartosz.budzanowski@gmail.com>
2016-11-17 16:28:36 +0100

    Copy reducer implementation from #9323

--- client/state/themes/reducer.js
+++ client/state/themes/reducer.js
@@ -11,6 +11,17 @@ import themeDetails from './theme-details/reducer';
 import themesList from './themes-list/reducer';
 import currentTheme from './current-theme/reducer';
 import themesUI from './themes-ui/reducer';
+import { createReducer } from 'state/utils';
+import {
+	THEME_ACTIVATE_REQUEST_SUCCESS,
+} from 'state/action-types';
+
+export const activeThemes = createReducer( {}, {
+	[ THEME_ACTIVATE_REQUEST_SUCCESS ]: ( state, { siteId, theme } ) => ( {
+		...state,
+		[ siteId ]: theme.id
+	} )
+} );
 
 export default combineReducers( {
 	themes,
